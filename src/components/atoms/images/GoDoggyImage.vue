1212b7881842eba88ea95bbb8579683ef495d6a1
lekas205 <lekass205@gmail.com>
2023-02-08 14:24:42 +0100

    implemeted image lazy loading

--- src/components/atoms/images/GoDoggyImage.vue
+++ src/components/atoms/images/GoDoggyImage.vue
@@ -1,5 +1,5 @@
 <template>
-    <img :src="url" alt="">
+    <img v-lazy="url" alt="">
 </template>
 
 <script lang="ts" setup>
@@ -15,5 +15,19 @@ img {
     width: 100%;
     height: 100%;
     margin-bottom: 24px;
+
+    &[lazy=loading] {
+        background: #eee;
+        background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+        border-radius: 8px;
+        background-size: 200% 100%;
+        animation: 1.5s shine linear infinite;
+    }
+
+    @keyframes shine {
+    to {
+        background-position-x: -200%;
+    }
+}
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import router from "./router";
 import http from './http/axios';
 import { store, key } from './store'
 import { AxiosKey } from './http/symbol';
+
 import './assets/scss/main.scss';
+
+import VueLazyLoad from 'vue3-lazyload'
 
 createApp(App)
     .use(router)
     .use(store, key)
+    .use(VueLazyLoad,{})
     .provide(AxiosKey, http)
     .mount('#app')
 
